83c2332a242aee283790296e7b116a0114b76c0b
hunputer <52592192+hunputer@users.noreply.github.com>
2020-12-09 15:37:13 +0900

    1

--- src/main/java/com/jh/yogiyo/member/memberController.java
+++ src/main/java/com/jh/yogiyo/member/memberController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 @Controller
 @RequestMapping("/member/**")
@@ -13,6 +14,13 @@ public class memberController {
 	public ModelAndView memberLogin() throws Exception{
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("member/memberLoginForm");
+		return mv;
+	}
+	
+	@GetMapping("memberJoin")
+	public ModelAndView memberJoin() throws Exception{
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("member/memberJoin");
 		return mv;
 	}
 	
