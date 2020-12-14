8e86a0a85b61ab9df7adba9d18ac0485afacc063
hunputer <52592192+hunputer@users.noreply.github.com>
2020-12-14 17:23:54 +0900

    1

---
+++ src/main/java/com/jh/yogiyo/store/detail/ReviewAplyVO.java
@@ -0,0 +1,16 @@
+package com.jh.yogiyo.store.detail;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class ReviewAplyVO {
+
+   private long num;
+   private long storeNum;
+   private long reviewNum;
+   private String id;
+   private String contents;
+   private Date regDate;
+}
---
+++ src/main/java/com/jh/yogiyo/store/detail/ToppingVO.java
@@ -0,0 +1,12 @@
+package com.jh.yogiyo.store.detail;
+
+import lombok.Data;
+
+@Data
+public class ToppingVO {
+   private long num;
+   private long storeNum;
+   private long menuNum;
+   private String toppingName;
+   private long toppingPrice;
+}
